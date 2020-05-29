fcc13c170d4a90890f86b75d31b286899a696d7e
zachary.care <zachary.care@protonmail.com>
2020-05-29 19:23:13 +0800

    login page

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/login">Login</router-link>
-    <router-view></router-view>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <style>
-  #app {
+  #nav {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    margin: 60px 0;
   }
 </style>
